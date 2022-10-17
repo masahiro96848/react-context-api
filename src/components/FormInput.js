a49dd70ec3084eeb2bb89838972af80a42d09cf6
masahiro96848 <feetwcpic9436@gmail.com>
2022-10-18 08:53:17 +0900

    feat: FormInput.jsでタスクが追加できるように実装

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { DataContext } from './DataProvider'
 
 export default function FormInput() {
+    const [todos, setTodos] = useContext(DataContext)
+    const [todoName, setTodoName] = useState('')
+
+    const addTodo = (e) => {
+        e.preventDefault()
+        setTodos([...todos, { name: todoName, complete: false }])
+        setTodoName('')
+    }
+
     return (
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={addTodo}>
             <input
                 type="text"
                 name="todos"
                 id="todos"
                 required
                 placeholder="what needs to be done?"
+                value={todoName}
+                onChange={(e) => setTodoName(e.target.value.toLowerCase())}
             />
             <button type="submit">Create</button>
         </form>
