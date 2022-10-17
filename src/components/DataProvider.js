305e2a70539aba98e31177cf66fc970c39121e64
masahiro96848 <feetwcpic9436@gmail.com>
2022-10-18 08:59:59 +0900

    feat: localStorageを追加

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -5,6 +5,15 @@ export const DataContext = createContext()
 export const DataProvider = (props) => {
     const [todos, setTodos] = useState([])
 
+    useEffect(() => {
+        const todoStore = JSON.parse(localStorage.getItem('todoStore'))
+        if (todoStore) setTodos(todoStore)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('todoStore', JSON.stringify(todos))
+    }, [todos])
+
     return (
         <DataContext.Provider value={[todos, setTodos]}>
             {props.children}
