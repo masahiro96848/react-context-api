9e1c8870f1b13ebab0bc88cc8d84cc1b9dbe8b76
masahiro96848 <feetwcpic9436@gmail.com>
2022-10-18 08:35:12 +0900

    feat: DataProviderファイルを作成。DataContextを作成

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 import FormInput from './components/FormInput'
 import List from './components/List'
 import Footer from './components/Footer'
+import { DataProvider } from './components/DataProvider'
 
 function App() {
     return (
-        <div className="App">
-            <h1>Todo List</h1>
-            <FormInput />
-            <List />
-            <Footer />
-        </div>
+        <DataProvider>
+            <div className="App">
+                <h1>Todo List</h1>
+                <FormInput />
+                <List />
+                <Footer />
+            </div>
+        </DataProvider>
     )
 }
 
