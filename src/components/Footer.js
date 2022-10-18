f6d93a81f8331aab85cdc491719e7676e22997c3
masahiro96848 <feetwcpic9436@gmail.com>
2022-10-19 00:16:34 +0900

    feat: todoのcomplete機能　削除機能を追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { DataContext } from './DataProvider'
 
 export default function Footer() {
+    const [checkAll, setCheckAll] = useState(false)
+    const [todos, setTodos] = useContext(DataContext)
+
+    const handleCheckAll = () => {
+        const newTodos = [...todos]
+        newTodos.forEach((todo) => {
+            todo.complete = !checkAll
+        })
+        setTodos(newTodos)
+        setCheckAll(!checkAll)
+    }
+
+    const deleteTodo = () => {
+        const newTodos = todos.filter((todo) => {
+            return todo.complete === false
+        })
+        setTodos(newTodos)
+        setCheckAll(false)
+    }
+
     return (
         <div className="row">
             <label htmlFor="all">
-                <input type="checkbox" name="all" id="" />
+                <input
+                    type="checkbox"
+                    name="all"
+                    id=""
+                    onChange={handleCheckAll}
+                    checked={checkAll}
+                />
                 ALL
             </label>
-            <p>You have 0 todo</p>
-            <button id="delete">Delete</button>
+            <p>You have {todos.length} todo</p>
+            <button id="delete" onClick={deleteTodo}>
+                Delete
+            </button>
         </div>
     )
 }
