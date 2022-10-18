993f69f06709f5efa952205ede44c7edca8f2d09
masahiro96848 <feetwcpic9436@gmail.com>
2022-10-18 09:23:40 +0900

    feat: todoの完了機能を追加

--- src/components/List.js
+++ src/components/List.js
@@ -5,10 +5,25 @@ import { DataContext } from './DataProvider'
 export default function List() {
     const [todos, setTodos] = useContext(DataContext)
 
+    const switchComplete = (id) => {
+        const newTodos = [...todos]
+        newTodos.forEach((todo, index) => {
+            if (index === id) {
+                todo.complete = !todo.complete
+            }
+        })
+        setTodos(newTodos)
+    }
+
     return (
         <ul>
             {todos.map((todo, index) => (
-                <ListItem todo={todo} key={index} id={index} />
+                <ListItem
+                    todo={todo}
+                    key={index}
+                    id={index}
+                    checkComplete={switchComplete}
+                />
             ))}
         </ul>
     )
